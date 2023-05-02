22c6dc28a8680e0981c9cca5129bf0647548233e
cyberin0 <cyberino@pm.me>
2023-05-02 13:54:18 -0700

    v2.2-base / Requires testing

--- text_summarization.py
+++
@@ -1,8 +0,0 @@
-# Import your text summarization library and implement the summarization function
-# This example uses the Gensim library
-
-from gensim.summarization import summarize
-
-def summarize_text(text):
-    summary = summarize(text)
-    return summary
