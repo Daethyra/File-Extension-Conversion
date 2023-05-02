2bea5e75e8fe9274409503f24621a3695d9127b5
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-05-02 14:08:39 -0700

    Delete parallel_processing.py

--- parallel_processing.py
+++
@@ -1,6 +0,0 @@
-import concurrent.futures
-
-def process_file_parallel(files, options, process_file_function):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        results = executor.map(process_file_function, files, [options] * len(files))
-    return list(results)
