0dc5810922bc6dce1fc143d4f1a30186531023d6
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-05-02 14:08:46 -0700

    Delete jpeg_processing.py

--- jpeg_processing.py
+++
@@ -1,8 +0,0 @@
-import pytesseract
-from PIL import Image
-
-def process_jpeg(file, delimiters):
-    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    img = Image.open(file)
-    text = pytesseract.image_to_string(img)
-    return text
