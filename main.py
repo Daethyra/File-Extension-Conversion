482938172e626c0dfe15cb604807e707fada2f9b
Daethyra <109057945+Daethyra@users.noreply.github.com>
2023-05-05 19:43:57 -0700

    new file:   extensions.py 	modified:   main.py 	new file:   utility.py

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 import os
 import sys
 import shutil
-import pypandoc
-import img2pdf
-from pdf2image.pdf2image import convert_from_path
-import fitz
 import requests
 from urllib.parse import urlparse
-
-pdflatex_path = r"C:\Users\{user}\AppData\Local\Programs\MiKTeX\miktex\bin\x64\pdflatex.exe"
+from extensions import Converter
 
 def is_url(input_path):
     try:
@@ -24,34 +19,17 @@ def download_file(url, local_path):
             for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
 
-def convert_to_pdf(input_file, input_extension, output_file):
-    print(f"Processing {input_file}...")
-
-    try:
-        if input_extension in ['.pdf', '.PDF']:
-            shutil.copy(input_file, output_file)
-        elif input_extension.lower() in ['.jpg', '.jpeg', '.png', '.bmp']:
-            with open(output_file, "wb") as pdf_file:
-                pdf_bytes = img2pdf.convert(input_file)
-                if pdf_bytes:
-                    pdf_file.write(pdf_bytes)
-                else:
-                    raise ValueError("Empty output")
-        else:
-            output = pypandoc.convert_file(input_file, 'pdf', outputfile=output_file, extra_args=['--pdf-engine', pdflatex_path, '--quiet'])
-    except Exception as e:
-        print(f"Error converting {input_file} to PDF: {str(e)}")
-        return
-
-    print(f"Converted {input_file} to {output_file}")
-
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python main.py <input_file>")
+    if len(sys.argv) != 3:
+        print("Usage: python main.py <input_file> <output_format>")
         sys.exit(1)
 
     input_path = sys.argv[1]
+    output_format = sys.argv[2].lower()
     
+    if not output_format.startswith('.'):
+        output_format = '.' + output_format
+
     if is_url(input_path):
         url_filename = os.path.basename(urlparse(input_path).path)
         input_file = f"temp_{url_filename}"
@@ -63,10 +41,24 @@ if __name__ == "__main__":
         print(f"Input file '{input_file}' does not exist.")
         sys.exit(1)
 
-    input_filename, input_extension = os.path.splitext(input_file)
-    output_file = f"{input_filename}_output.pdf"
+    input_filename = os.path.splitext(input_file)[0]
+    output_file = f"{input_filename}_output{output_format}"
+    
+    converter = Converter(input_file)
 
-    convert_to_pdf(input_file, input_extension, output_file)
+    if output_format == '.pdf':
+        converter.to_pdf(output_file)
+    elif output_format == '.json':
+        converter.to_json(output_file)
+    elif output_format == '.csv':
+        converter.to_csv(output_file)
+    elif output_format == '.yaml':
+        converter.to_yaml(output_file)
+    else:
+        print(f"Unsupported output format: {output_format}")
+        sys.exit(1)
+
+    print(f"Converted {input_file} to {output_file}")
 
     if is_url(input_path):
         os.remove(input_file)  # Clean up the temporary file
