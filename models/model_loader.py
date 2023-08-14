9407a624707bda813eaa514f18f923fa6f00db91
Daemon <109057945+Daethyra@users.noreply.github.com>
2023-08-13 19:32:13 -0700

    Fresh start.

--- models/model_loader.py
+++
@@ -1,9 +0,0 @@
-from transformers import AutoModelForSequenceClassification, AutoTokenizer
-
-def load_model(model_path):
-    """
-    Load a locally saved language model.
-    """
-    model = AutoModelForSequenceClassification.from_pretrained(model_path)
-    tokenizer = AutoTokenizer.from_pretrained(model_path)
-    return model, tokenizer
